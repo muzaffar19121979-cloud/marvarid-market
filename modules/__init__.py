c3fa39b8f36187e34d4b57bbb3c9c0a918d329a3
muzaffar19121979-cloud <muzaffar19121979@gmail.com>
2026-07-17 13:29:29 +0500

    тест 4

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,22 +4,3 @@ from modules.sales import SalesManager
 from modules.credits import CreditManager
 from modules.cash import CashManager
 from modules.reports import ReportManager
-from modules.printer import PrinterManager
-from modules.label_printer import LabelPrinter
-
-try:
-    from modules.qr_scanner import BarcodeScanner
-except ImportError:
-    BarcodeScanner = None
-    print("⚠️ QR сканер мавжуд эмас")
-
-__all__ = [
-    'ProductManager',
-    'SalesManager',
-    'CreditManager',
-    'CashManager',
-    'ReportManager',
-    'PrinterManager',
-    'LabelPrinter',
-    'BarcodeScanner',
-]
