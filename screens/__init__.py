be2051cb43fdc04aa39a25933efda7f0e04b9018
muzaffar19121979-cloud <muzaffar19121979@gmail.com>
2026-07-17 10:50:43 +0500

    тест2

--- screens/__init__.py
+++ screens/__init__.py
@@ -1,18 +1,6 @@
-# screens/__init__.py
 from screens.main_screen import MainScreen
 from screens.sales_screen import SalesScreen
 from screens.products_screen import ProductsScreen
 from screens.credits_screen import CreditsScreen
 from screens.cash_screen import CashScreen
 from screens.report_screen import ReportScreen
-from screens.dialog_mixin import DialogMixin
-
-__all__ = [
-    'MainScreen',
-    'SalesScreen',
-    'ProductsScreen',
-    'CreditsScreen',
-    'CashScreen',
-    'ReportScreen',
-    'DialogMixin',
-]
--- screens/products/__init__.py
+++
@@ -1,6 +0,0 @@
-# screens/products/__init__.py
-from screens.products.product_form import ProductFormMixin
-from screens.products.product_labels import ProductLabelsMixin
-from screens.products.product_qr import ProductQRMixin
-
-__all__ = ['ProductFormMixin', 'ProductLabelsMixin', 'ProductQRMixin']
